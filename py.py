c3441825549d02fb6447743e23bee15c31e2a735
HowToPlayMeow <gamebossmc682@gmail.com>
2023-09-15 12:19:04 +0700

    code v.1 : Meow

---
+++ py.py
@@ -0,0 +1,6 @@
+def funcA(x = 5, y = 20, z = 10) :
+    print(x + y + z)
+    print("Meow")
+
+funcA(10, 20)
+funcA(5, 6, 7)
---
+++ py2.py
@@ -0,0 +1,12 @@
+def func1( ) :
+    print('AAA')
+    print('BBB')
+    return
+    print('CCC')
+
+def func2( x ) :
+    return
+    print(f'X is (x)')
+
+func1( )
+func2( 10 )
